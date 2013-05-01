87c72caa809e4957234d409a9828101e2dc56ee9
Ayose <ayosec@gmail.com>
2013-05-01 10:28:48 +0100

    Fix something

--- src/main/scala/com/ayosec/wikispray/moon/Exceptions.scala
+++ src/main/scala/com/ayosec/wikispray/moon/Exceptions.scala
@@ -2,3 +2,6 @@ package com.ayosec.wikispray.moon
 
 class MoonError extends Exception
 class DocumentNotFound(val collection: MoonCollection, val query: com.mongodb.DBObject) extends MoonError
+
+
+// More
